ea4da8a5aa29d4c9e054a04401deea936979f44c
freezzze <rada611@gmail.com>
2023-11-10 10:34:56 +0300

    edit file

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -216,8 +216,8 @@ resource "yandex_compute_instance" "ci" {
 
   resources {
     core_fraction = 20
-    cores         = 2
-    memory        = 2
+    cores         = 4
+    memory        = 4
   }
 
   boot_disk {
