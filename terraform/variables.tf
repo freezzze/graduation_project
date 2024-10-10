98a7b9885b5089010d3c6ee0907e9311ca5b0309
freezzze <rada611@gmail.com>
2024-10-10 16:23:06 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,16 +2,15 @@
 
 variable "yandex_token" {
   type        = string
-  default     = "AQAAAAAF7yWRAATuwVutZjM00UeFsI3NFOg4ziw"
+  default     = "your yandex_token"
 }
 variable "yandex_cloud_id" {
   type        = string
-#default     = "b1gnrhc5ps4umgn2srhi"
-default     = "fd8sqojvm458b3jr5nfd"
+default     = "youe yandex_cloud_id"
 }
 variable "yandex_folder_id" {
   type        = string
-  default     = "b1gjo8e1mntt7chmpk62"
+  default     = "your yandex_folder_id"
 }
 variable "yandex_zone" {
   type        = string
